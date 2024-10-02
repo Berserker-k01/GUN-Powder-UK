72ffcf6605f4a0f6d61e2c4554feca2368b8de5e
Berserker <155231526+Berserker-k01@users.noreply.github.com>
2024-10-02 20:56:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Utiliser le port 3000 pour le développement
+    strictPort: true // Empêche l'utilisation d'un autre port si 3000 est occupé
+  },
+});
